8cfd2800a0e445aedf4f67b8617da50247cb8468
quandram <james.longman@gmail.com>
2015-03-20 13:08:44 +0000

    JL: Completed functions with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code file contains two funcitons.  One to construct a custom Matrix 
+## capable of storing its inverse and the other to retrieve that inverse if it
+## exists or to create it.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix - custom Matrix with four internal functions
+## - getMatrix :- returns the Matrix
+## - setMatrix :- sets the Matrix
+## - getInverse :- returns the inverse of the Matrix
+## - setInverse :- sets the inverse of the Matrix
+makeCacheMatrix <- function(thisMatrix = matrix()) {
+    thisInverse <- NULL
+        
+    ## function to store the Matrix
+    setMatrix <- function(newMatrix) {
+        ## avoids storing the Matrix and clearing the inverse if the supplied
+        ## Matrix and the stored Matrix are identical
+        if (dim(thisMatrix) == dim(newMatrix) && all(thisMatrix == newMatrix)){
+            message("Matrix has not changed, preserving cached values")
+        }
+        else{
+            thisMatrix <<- newMatrix
+            thisInverse <<- NULL
+        }
+    }
+    
+    getMatrix <- function() thisMatrix
+    
+    setInverse <- function(theInverse) thisInverse <<- theInverse
+    
+    getInverse <- function() thisInverse
+    
+    list(setMatrix = setMatrix, 
+         getMatrix = getMatrix, 
+         setInverse = setInverse, 
+         getInverse = getInverse,)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - Returnes the inverse of a given custom Matrix object.
+cacheSolve <- function(thisMatrix, ...) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## retrieves stored inverse and checks for content to avoid unnecessary
+    ## recalculation 
+    thisInverse <- thisMatrix$getInverse()
+    if(!is.null(thisInverse)) {
+        message("Retrieving cached data")
+        # exit function returning existing value
+        return(thisInverse)
+    }
+    
+    ## calculates, stores and returns inverse for new Matrix
+    thisInverse <- solve(thisMatrix$getMatrix(), ...)
+    thisMatrix$setInverse(thisInverse)
+    thisInverse
 }
